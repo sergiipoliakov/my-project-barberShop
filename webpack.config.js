cca78b1900ac90e5e714e25859a3cdc20e2360d9
sergiipoliakov <64687462+sergiipoliakov@users.noreply.github.com>
2020-12-05 14:15:43 +0200

    add webpack and seting

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+	entry: "./src/index.js",
+	output: {
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "build"),
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /node_modules/,
+				use: ["babel-loader"],
+			},
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+			{ test: /\.hbs$/, loader: "handlebars-loader" },
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({ template: "src/index.html" }),
+		new MiniCssExtractPlugin(),
+	],
+	devServer: {
+		contentBase: path.join(__dirname, "build"),
+		compress: true,
+		port: 9000,
+	},
+};
